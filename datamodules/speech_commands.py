47defb95ed1498eb83ec5fea4304c43f4b2b21f9
TrellixVulnTeam <charles.mcfarland@trellix.com>
2022-10-06 12:12:29 +0000

    Adding tarfile member sanitization to extractall()

--- datamodules/speech_commands.py
+++ datamodules/speech_commands.py
@@ -143,7 +143,26 @@ class SpeechCommandsDataModule(pl.LightningDataModule):
             download_file_path,
         )
         with tarfile.open(download_file_path, "r") as f:
-            f.extractall(self.download_location)
+            def is_within_directory(directory, target):
+                
+                abs_directory = os.path.abspath(directory)
+                abs_target = os.path.abspath(target)
+            
+                prefix = os.path.commonprefix([abs_directory, abs_target])
+                
+                return prefix == abs_directory
+            
+            def safe_extract(tar, path=".", members=None, *, numeric_owner=False):
+            
+                for member in tar.getmembers():
+                    member_path = os.path.join(path, member.name)
+                    if not is_within_directory(path, member_path):
+                        raise Exception("Attempted Path Traversal in Tar File")
+            
+                tar.extractall(path, members, numeric_owner=numeric_owner) 
+                
+            
+            safe_extract(f, self.download_location)
 
     def _process_data(self):
         if not os.path.exists(self.data_processed_location):
